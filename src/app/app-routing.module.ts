3d5720c77e24f28d4ad70bbbfa057573a22783fc
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-06-23 15:45:10 +0200

    Scenario y more services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+/*   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  }, */
   {
     path: 'patient',
     loadChildren: () => import('./patient/patient.module').then( m => m.PatientPageModule)
+  },
+  {
+    path: 'scenarios',
+    loadChildren: () => import('./scenarios/scenarios.module').then( m => m.ScenariosPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./tab0/tab0.module').then( m => m.Tab0PageModule)
   }
 ];
 @NgModule({
